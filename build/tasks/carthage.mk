5200235098dea2a67b0d45869118566b18ffc536
Clashkid155 <frankdiss9@gmail.com>
2020-03-23 14:42:56 +0100

    Need only integer

--- build/tasks/carthage.mk
+++ build/tasks/carthage.mk
@@ -24,6 +24,6 @@ carthage: $(INTERNAL_OTA_PACKAGE_TARGET)
 	@echo -e "\t Zip: $(CONQUER_TARGET_PACKAGE)"
 	@echo -e "\t MD5: `cat $(CONQUER_TARGET_PACKAGE).md5sum | awk '{print $$1}' `"
 	@echo -e "\t Size: `du -sh $(CONQUER_TARGET_PACKAGE) | awk '{print $$1}' `"
-	@echo -e "\t Version: $(CONQUER_VERSION)"
+	@echo -e "\t Version: $(CONQUER_VERSION) | awk -F: '{split($0,con,"-"); print con[2]}'
 	@echo -e "\t ConquerOS X - BeTheConqueror"
 	@echo -e "\t =============================================================================================================="
